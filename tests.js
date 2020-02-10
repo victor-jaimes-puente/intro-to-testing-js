1a729003ddd11d86c29915f26f5043e3060d3170
Victor Jaimes-Puente <victor.jaimes.puente@gmail.com>
2020-02-10 14:26:20 -0600

    finishedEx1

--- tests.js
+++ tests.js
@@ -13,3 +13,17 @@ describe('helloWorld', function() {
       expect(helloWorld()).not.toBe(undefined);
     });
 });
+describe('sayHello', function () {
+    it('should be a defined function', function () {
+        expect(typeof sayHello).toBe('function')
+    });
+    it('should return a string when called', function () {
+        expect(typeof sayHello()).toBe('string')
+    });
+    it('should return the string "hello" when called', function () {
+        expect(sayHello()).toBe("hello")
+    });
+    it('should never return undefined when called', function () {
+        expect(sayHello()).not.toBe(undefined)
+    });
+});
